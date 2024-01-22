9fce2212929aef10a614aa410aa5ebea9e83644f
lunqwe <roma.boyko10068@gmail.com>
2024-01-22 22:39:53 +0200

    12131341

--- config/config/wsgi.py
+++ config/config/wsgi.py
@@ -7,8 +7,9 @@ For more information on this file, see
 https://docs.djangoproject.com/en/5.0/howto/deployment/wsgi/
 """
 
-import os
+import os, sys
 from django.core.wsgi import get_wsgi_application
 
+sys.path.append('/config/config')
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.config.settings')
 application = get_wsgi_application()
