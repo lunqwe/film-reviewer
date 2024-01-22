9782af115baecf0cfb501c0fd12c81ba6e17b41d
lunqwe <roma.boyko10068@gmail.com>
2024-01-22 23:59:51 +0200

    static collection

--- config/config/settings.py
+++ config/config/settings.py
@@ -157,8 +157,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
-STATIC_ROOT = "/config/static/"
+STATIC_ROOT = "D:\.prog\jobpilot\Jobpilot-server\config\static"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
