f4e07c72f60de648c10c587545a6696fc10147b6
lunqwe <roma.boyko10068@gmail.com>
2024-01-22 22:49:43 +0200

    dshfifhsifhsfsdfs

--- config/config/wsgi.py
+++ config/config/wsgi.py
@@ -10,6 +10,6 @@ https://docs.djangoproject.com/en/5.0/howto/deployment/wsgi/
 import os, sys
 from django.core.wsgi import get_wsgi_application
 
-sys.path.append('/config/config')
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.config.settings')
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings')
 application = get_wsgi_application()
