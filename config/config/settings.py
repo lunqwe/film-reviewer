3c988e94e6363429b72792129accbc26e632b2ca
lunqwe <roma.boyko10068@gmail.com>
2024-01-25 13:51:59 +0200

    variable setting

--- config/config/settings.py
+++ config/config/settings.py
@@ -162,8 +162,8 @@ USE_TZ = True
 
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = "smtp.sendgrid.net"
-EMAIL_HOST_USER = config('EMAIL_HOST_USER')
-EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD')
+EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = os.environ.get("EMAIL_HOST_PASSWORD")
 EMAIL_PORT = 465 
 
 
