88b3d03e49fafb627133c5ca588a50c2866edc5f
lunqwe <roma.boyko10068@gmail.com>
2024-01-29 21:13:18 +0200

    fix

--- config/user/views.py
+++ config/user/views.py
@@ -102,7 +102,7 @@ class VerifyEmailView(generics.CreateAPIView):
         if user:
             if not check_verified:
                 generated_code = random.randint(100000,1000000)
-                verificator = Verificator.objects.get(user=user)
+                verificator = Verificator.objects.create(user=user)
                 print(generated_code)
                 verificator.code = str(generated_code)
                 verificator.save()
