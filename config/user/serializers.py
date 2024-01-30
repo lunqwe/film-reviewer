d5189732d4e02b1d8dd288ec02a4b65e1aacf7e3
lunqwe <roma.boyko10068@gmail.com>
2024-01-30 22:33:53 +0200

    fix

--- config/user/serializers.py
+++ config/user/serializers.py
@@ -109,7 +109,7 @@ class ResetPasswordRequestSerializer(serializers.Serializer):
         user = CustomUser.objects.get(id=self.user_id)
         if user:
             user_uidb64 = urlsafe_base64_encode(str(user.pk).encode('utf-8')) # encode 
-            user_token = Token.objects.get(user=user)
+            user_token = Token.objects.get(user=user).token
             return [user_uidb64, user_token]
 
             
