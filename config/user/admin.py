a4db23058e6bd504bea1bb77bce5e20208442a4b
lunqwe <roma.boyko10068@gmail.com>
2024-02-16 16:03:01 +0200

    admin panel settings changes

--- config/user/admin.py
+++ config/user/admin.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
-from .models import CustomUser, Verificator, Employer, Candidate
+from .models import CustomUser, Verificator, Employer, Candidate, ResumeFile
 
 admin.site.register(CustomUser)
 admin.site.register(Verificator)
 admin.site.register(Employer)
 admin.site.register(Candidate)
+admin.site.register(ResumeFile)
 
