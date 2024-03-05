81f2ecc01bd1657f36c62ea15b8fa33e6236074d
lunqwe <roma.boyko10068@gmail.com>
2024-03-05 14:29:53 +0200

    filters fix

--- config/vacancy/filters.py
+++ config/vacancy/filters.py
@@ -2,9 +2,9 @@ import django_filters
 from .models import Vacancy
 
 class VacancyFilter(django_filters.FilterSet):
-    author = django_filters.CharFilter(field_name='company_name')
-    candidate = django_filters.CharFilter(field_name='candidates__id')
+    employer = django_filters.CharFilter(field_name='employer__user__id')
+    candidate = django_filters.CharFilter(field_name='candidates__user__id')
 
     class Meta:
         model = Vacancy
-        fields = ['employer_id', 'candidates']
+        fields = ['employer', 'candidates']
